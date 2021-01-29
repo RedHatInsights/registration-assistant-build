6f7141e3e83246b360509e632d49f86514e12ffd
Travis CI User <travis@example.org>
2021-01-29 12:41:30 +0000

    Update deps, fec v2 👉 v3 🥰🥰🥰🥰🥰 (#27)
    https://github.com/RedHatInsights/frontend-components/blob/master/packages/components/README.md#migration-v2---v3 IS THE WAY

--- js/374.d4e2229b6dd0a5c9fcc7.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkregistration_assistant=self.webpackChunkregistration_assistant||[]).push([[374],{1374:(e,a,t)=>{"use strict";t.r(a),t.d(a,{default:()=>u});var n=t(1037),s=t(7605),r=t(7522),o=t.n(r),l=t(5399),i=t(811),c=t(1806);const u=function(){return o().createElement(s.IntlProvider,{locale:navigator.language.slice(0,2),messages:c,onError:console.log},o().createElement(l.BrowserRouter,{basename:(0,i.getBaseName)(window.location.pathname)},o().createElement(n.Z,null)))}}}]);
-//# sourceMappingURL=374.d4e2229b6dd0a5c9fcc7.js.map
