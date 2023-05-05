56bf786623383961db09442c11eb8df7ffd31ee0
Travis CI User <travis@example.org>
2023-05-05 10:07:26 +0000

    Fix link for "View Insights Dashboard" (#68)
    Co-authored-by: Michael Johnson <micjohns@newbeginnings.usersys.redhat.com>

---
+++ js/446.1683281188286.982f931461cef84ae28b.js
@@ -0,0 +1,2 @@
+(self.webpackChunkregistration_assistant=self.webpackChunkregistration_assistant||[]).push([[446],{18446:(s,t,a)=>{var n=a(90939);s.exports=function(s,t){return n(s,t)}}}]);
+//# sourceMappingURL=../sourcemaps/446.eb0bb6bdca0dc5c3820b52ecf4384d74.js.map
